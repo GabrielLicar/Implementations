cc5ade37c01e5120b37d4d2f2db5e36cedf4a474
Gabriel Licar <114968559+GabrielLicar@users.noreply.github.com>
2022-12-28 19:21:47 -0300

    Changes

--- register.js
+++
@@ -1,9 +0,0 @@
-function consultar() {
-    const id = document.getElementById("id-consulta").value;
-    fetch(`http://localhost:8080/parking-spot/${id}`)
-        .then(response => response.json())
-        .then(data => console.log(`SpotNumber: ${response.parkingSpotNumber}`));
-}
-
-
-      
